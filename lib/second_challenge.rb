18e02eece51e4b420f4814ee8d6365c1f5db0eef
Matt Orvin <matthew.orvin@gmail.com>
2019-04-26 21:50:08 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,3 +1,4 @@
+require "pry"
 def second_challenge
   groceries = {
    dairy: ["milk", "yogurt", "cheese"],
@@ -7,6 +8,7 @@ def second_challenge
   }
 
   #code your solution here!
+  binding.pry
 groceries.values
   
 
